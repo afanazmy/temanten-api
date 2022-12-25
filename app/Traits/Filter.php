d80fcaa55e6381bb9defc3c865d49f547d55e015
afanazmy <afan123azmy@gmail.com>
2022-12-25 21:05:13 +0700

    [ADD] Filter - show deleted

--- app/Traits/Filter.php
+++ app/Traits/Filter.php
@@ -4,6 +4,7 @@ namespace App\Traits;
 
 use Illuminate\Http\Request;
 use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\Schema;
 
 trait Filter
 {
@@ -17,7 +18,7 @@ trait Filter
      */
     public function filter(Request $request, Builder $query, $config = ['paginate' => true])
     {
-        $_request = $request->except(['pagination', 'token', 'search']);
+        $_request = $request->except(['pagination', 'token', 'search', 'show_deleted']);
 
         /**
          * For search in spesific columns,
@@ -34,7 +35,7 @@ trait Filter
          * we use OR to filter data.
          */
         $columns = $query->columns;
-        $search = $request->search ?? null;
+        $search = $request->search ?? false;
         if ($search) {
             $query->orWhere(function (Builder $query) use ($columns, $search) {
                 foreach ($columns as $column) {
@@ -43,9 +44,20 @@ trait Filter
             });
         }
 
+        /**
+         * Handle tables with soft deletes
+         */
+        $showDeleted = $request->show_deleted ?? false;
+        $hasDeletedAt = Schema::hasColumn($query->from, 'deleted_at');
+        if ($hasDeletedAt) {
+            $where = 'whereNull';
+            if ($showDeleted) $where = 'orWhereNotNull';
+            $query->$where('delete');
+        }
+
         $pagination = $request->pagination ?? 10;
         if ($config['paginate']) {
-            return $query->paginate($pagination);
+            return $query->simplePaginate($pagination);
         }
 
         return $query->get();
