826666e6defec379047b2f9319ea8f84cda24ddb
afanazmy <afan123azmy@gmail.com>
2022-12-25 21:07:39 +0700

    [ADD] Invitation - migration

---
+++ database/migrations/2022_12_25_174628_create_invitations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('recipient_name');
+            $table->tinyInteger('is_group');
+            $table->string('deleted_by')->nullable();
+            $table->dateTime('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invitations');
+    }
+};
