3fea009c0e55eb946489882b50c6ad5d42034c42
afanazmy <afan123azmy@gmail.com>
2022-12-25 21:07:47 +0700

    [ADD] Guest Book - migration

---
+++ database/migrations/2022_12_25_180038_create_guest_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guest_books', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('invitation_id')->constrained('invitations')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('number_of_guest');
+            $table->tinyInteger('is_group');
+            $table->string('deleted_by')->nullable();
+            $table->dateTime('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guest_books');
+    }
+};
