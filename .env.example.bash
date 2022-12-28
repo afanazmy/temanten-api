c1e64b92954c9cb340b9985cc61cc798ca05ec02
afanazmy <afan123azmy@gmail.com>
2022-12-28 09:44:54 +0700

    [ADD] .env.example.bash - for jenkins

---
+++ .env.example.bash
@@ -0,0 +1,19 @@
+APP_NAME='Temanten App'
+APP_ENV=local
+APP_KEY=
+APP_DEBUG=false
+APP_URL=$app_url
+APP_TIMEZONE='Asia/Jakarta'
+
+LOG_CHANNEL=stack
+LOG_SLACK_WEBHOOK_URL=
+
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=$db_name
+DB_USERNAME=$db_user
+DB_PASSWORD=$db_password
+
+CACHE_DRIVER=file
+QUEUE_CONNECTION=sync
