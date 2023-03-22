f70118074bbdd49e9c1aef8c2c3c4ea9ab30ba30
afanazmy <m.afan.azmi@mail.ugm.ac.id>
2023-03-22 10:57:58 +0700

    [ADD] Setup Wizard - index

--- app/Http/Controllers/SetupWizardController.php
+++ app/Http/Controllers/SetupWizardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Date;
@@ -22,9 +23,16 @@ class SetupWizardController extends Controller
         $this->language = new Language(Auth::user());
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $result = DB::table('setup_wizards')->where('status', SetupWizard::S_NOTYET)->get();
+        $result = DB::table('setup_wizards')->where('status', SetupWizard::S_NOTYET);
+
+        if ($request->excepts ?? false) {
+            $result->whereNotIn('type', $request->excepts);
+        }
+
+        $result = $result->get();
+
         return response()->json(
             DefaultResponse::parse('success', $this->language->get(Language::common['success']), $result)
         );
@@ -57,25 +65,6 @@ class SetupWizardController extends Controller
 
         DB::table('user_permissions')->insert($userPermissions);
 
-        // create token for apps
-        $personalAccessClients = [
-            [
-                'id' => Str::orderedUuid(),
-                'name' => 'Invitation App',
-                'token' => bin2hex(random_bytes(40)),
-                'created_at' => Date::now(),
-                'updated_at' => Date::now(),
-            ],
-            [
-                'id' => Str::orderedUuid(),
-                'name' => 'QR Code Scanner App',
-                'token' => bin2hex(random_bytes(40)),
-                'created_at' => Date::now(),
-                'updated_at' => Date::now(),
-            ],
-        ];
-        DB::table('personal_access_clients')->insert($personalAccessClients);
-
         DB::table('setup_wizards')->where('type', SetupWizard::T_SUPERADMIN)->update([
             'status' => SetupWizard::S_DONE,
             'updated_at' => Date::now()
@@ -83,13 +72,10 @@ class SetupWizardController extends Controller
 
         DB::commit();
 
-        $result = ['personal_access_clients' => $personalAccessClients];
-
         return response()->json(
             DefaultResponse::parse(
                 'success',
-                $this->language->get(Language::setupWizard['store'], ['appName' => env('APP_NAME')]),
-                $result
+                $this->language->get(Language::setupWizard['store'], ['appName' => env('APP_NAME')])
             )
         );
     }
